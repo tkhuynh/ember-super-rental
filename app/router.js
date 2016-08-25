48ef95cc9dbf688edc809c99332c93e5ae690309
tkhuynh <thequest2311@yahoo.com>
2016-08-25 15:04:49 -0700

    add about route

--- app/router.js
+++ app/router.js
@@ -7,6 +7,18 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('about'); // from ember g route about
 });
 
 export default Router;
+
+/*
+    Ember CLI uses ECMAScript 2015 (ES2015 for short or previously known as ES6) 
+    modules to organize application code. For example, the line import Ember 
+    from 'ember'; gives us access to the actual Ember.js library as the variable 
+    Ember. And the import config from './config/environment'; line gives us 
+    access to our app's configuration data as the variable config. const is a 
+    way to declare a read-only variable, as to make sure it is not accidentally 
+    reassigned elsewhere. At the end of the file, export default Router; makes 
+    the Router variable defined in this file available to other parts of the app.
+*/
