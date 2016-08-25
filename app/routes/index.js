8dd58f184ac8d8e2735120e664ee2aa0005b1e22
tkhuynh <thequest2311@yahoo.com>
2016-08-25 15:50:31 -0700

    load data from model

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+	model() {
+		return this.get('store').findAll('rental');
+		// this.get('store').findAll('rental'), Ember Data will make a GET request to /rentals
+		// https://guides.emberjs.com/v2.7.0/tutorial/ember-data/
+	}
 });
